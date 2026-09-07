f0417379b59f2f9bcf97038aecfe013252001056
Fildrance <fildrance@gmail.com>
2026-09-07 18:49:51 +0000

    Update command now accepts sha as argument (#10)
    * feat: ability to modify the primary outputted changelog
    
    * docs: add new variable to README.MD
    
    * refactor!: rename PRIMARY_CATEGORY to PRIMARY_CHANGELOG
    
    * refactor: change the name in the function too
    
    * refactor: switch UpdateWithMultipleChangeTypesInOnePREntry to theory
    
    * style: adjust comment to make test more parsable
    
    * refactor: replace other instances of hardcoded "Changelog"
    
    * fix: throw exception if local history does not exist (we have shallow checkout), and handle more complex tree by checking file existing in each commit
    
    * fix: null login for deleted users
    
    * fix: change order of items in 'change' items of changelog entry
    
    * refactor: additional test for parsing record with no user info
    
    * refactor: tests for LocalGitRepositoryTest
    
    * refactor: fix GitHubPullRequest model
    
    * feat: accept sinceSha as argument to pull list of changes on update command
    
    * refactor: handle non-existing .yml changelog files, change test to put data into non existing changelog file
    
    * refactor: update documentation, xml-doc/comments
    
    * refactor: hide non existing changelog file creation behind argument
    
    * refactor: add readme entry for new argument
    
    ---------
    
    Co-authored-by: Falcon <falcon@zigtag.dev>

--- SS14.ChangelogTool/Commands/UpdateCommand.cs
+++ SS14.ChangelogTool/Commands/UpdateCommand.cs
@@ -13,8 +13,16 @@ public sealed class UpdateCommand : Command
             Description = "Path to the changelog directory inside repository folder",
             Required = true,
         };
-
         Options.Add(changelogDirOption);
+
+        var sinceShaOption = new Option<string?>("--since-sha", "-s")
+        {
+            Description = "sha since which changes should be collected. "
+                          + "If not provided - last commit that changed "
+                          + "any of accepted Changelog files will be used instead",
+            Required = false
+        };
+        Options.Add(sinceShaOption);
 
         var allowCreateOption = new Option<bool>("--allow-create", "-ac")
         {
@@ -27,9 +35,11 @@ public sealed class UpdateCommand : Command
         SetAction(async parseResult =>
         {
             var changeLogDir = parseResult.GetValue(changelogDirOption)!;
+            var sinceSha = parseResult.GetValue(sinceShaOption);
             var allowCreate = parseResult.GetValue(allowCreateOption)!;
+
             return await changelogGenerator.TryGenerate(
-                extraCategories => changelogFileManager.GetLastMergedSha(changeLogDir, extraCategories),
+                extraCategories => sinceSha ?? changelogFileManager.GetLastMergedSha(changeLogDir, extraCategories),
                 (changelogs, revertedPrNumbers) => changelogFileManager.UpdateChangelogs(changelogs, revertedPrNumbers, changeLogDir, allowCreate)
             ) ? 0 : 1;
         });
